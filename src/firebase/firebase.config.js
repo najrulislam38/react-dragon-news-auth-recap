3ab28a2302cf54b9363dee848556927a7c206b5b
Najrul <mdnajrulislam.4738@gmail.com>
2023-10-04 12:12:22 +0600

    I have keep firebase key into .env.local file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD5Zp35FmNS250oZq_O3vJ1SLRT0EF9sas",
-  authDomain: "the-dragon-news-auth-3dc3f.firebaseapp.com",
-  projectId: "the-dragon-news-auth-3dc3f",
-  storageBucket: "the-dragon-news-auth-3dc3f.appspot.com",
-  messagingSenderId: "159460690747",
-  appId: "1:159460690747:web:46a2612c33c7ab2ecf051e",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
